33d483932ed83037ea2639f728dd77513385d9f5
salaxavier <salaxavier@outlook.com>
2020-04-07 01:05:30 +0100

    validation working

--- certificate/deploy.js
+++ certificate/deploy.js
@@ -3,7 +3,7 @@ const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
 const provider = new HDWalletProvider( //pass two arguments:
-  'zero gas grant exhaust remove brush edge fatigue stuff menu only upset', //Account mneumonic
+  'zero gas grant exhaust remove brush edge fatigue stuff menu only upset', //Account mnemonic
   'https://rinkeby.infura.io/v3/129084ddf049421185fde36759daa37c' //Infura API to connect to the Ethereum Rinkeby network
 );
 const web3 = new Web3(provider);
@@ -11,7 +11,7 @@ const web3 = new Web3(provider);
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]); //first account generated from mneumonic
+  console.log('Attempting to deploy from account', accounts[0]); //first account generated from mnemonic
 
   const result = await new web3.eth.Contract(JSON.parse(interface)) //interface = ABI  result: an instance of the contract
     .deploy({ data: bytecode })
--- src/Validation_parse.js
+++ src/Validation_parse.js
@@ -6,13 +6,12 @@ import {
 } from "react-router-dom";
 import web3 from "./web3";
 import certificate from "./certificate";
-import Validation from "./Validation_engine";
-var sha256 = require('js-sha256');
 
-export default function QueryParamsRout() {
+
+export default function QueryParamsExample() {
   return (
     <Router>
-      <QueryParams />
+      <QueryParamsDemo />
     </Router>
   );
 }
@@ -21,18 +20,13 @@ function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
 
-function QueryParams() {
+function QueryParamsDemo() {
   let query = useQuery();
-
-  var hash_data = query.get("name") + query.get("title") + query.get("type") + query.get("university") + query.get("graduation");
-  var hash = sha256.create();
-  hash.update(hash_data);
-  hash.hex();
 
   return (
     <div>
       <div>
-        <h1>Certificate Data</h1>
+        <h1>Certificate Validation</h1>
         <hr />
         <p><strong>Name:</strong> {query.get("name")} </p>
         <p><strong>Course:</strong> {query.get("title")} </p>
@@ -41,12 +35,8 @@ function QueryParams() {
         <p><strong>Year of Graduation:</strong> {query.get("graduation")} </p>
         <p><strong>Certificate Issuer:</strong> {query.get("issuer")} </p>
         <p><strong>Transaction ID:</strong> {query.get("transaction")} </p>
-        <p><strong>Recomputed hash:</strong> {hash.hex()} </p>
-        <hr />
-        <p id="root">
-        </p>
+        <button>Validate</button>
       </div>
-      ReactDOM.render(<Validation />, document.getElementById('root'));
     </div>
   );
 }
